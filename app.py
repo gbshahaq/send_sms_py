cb980679c57abde9f52d636bca337947d0ae3c0c
Shamsul Haque <Sham.Haque@lego.com>
2020-03-04 14:14:08 +0000

    function for call create

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 from phonenumbers import parse, phonenumberutil
-from vars import TWILIO_ACCOUNT_SID, TWILIO_NUMBER, TWILIO_AUTH_TOKEN, TWILIO_MSG_SVC
+from vars import TWILIO_ACCOUNT_SID, TWILIO_NUMBER, TWILIO_AUTH_TOKEN, TWILIO_MSG_SVC, TWILIO_NUMBER_US
 from vars import SIP_NUMBER
 from body import BODY
 
 # establish Twilio client
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
+# primary send - use messaging service
+def priSend(recip):
+    message = client.messages.create(
+    body=BODY,     
+    messaging_service_sid=TWILIO_MSG_SVC,
+    to=recip
+    )
+    print(message.sid)
+
+# secondary send - use a US SMS-enabled number
+def secSend(recip):
+    message = client.messages.create(
+    body=BODY, 
+    from_=TWILIO_NUMBER_US,
+    to=recip
+    )
+    print(message.sid)
 #open file for reading
 recipients = open("recip_list.txt","r")
 
@@ -16,15 +33,16 @@ recipients = open("recip_list.txt","r")
 for line in recipients:
         try:
             x = parse(line, None)
-            try:
-                message = client.messages.create(
-                    body=BODY, 
-#                    from_=TWILIO_NUMBER,
-                    messaging_service_sid=TWILIO_MSG_SVC,
-                    to=line
-                    )
-            except TwilioRestException:
-                print("Twilio REST error with number " + line)
+            if(line[0:2]) == "+1":
+                try:
+                    secSend(line)
+                except TwilioRestException:
+                    print("Twilio REST error with number " + line)
+            else:
+                try:
+                    priSend(line)
+                except TwilioRestException:
+                    print("Twilio REST error with number " + line)            
         except phonenumberutil.NumberParseException:
             print(line + " does not look like a phone number in E.164 format")      
 
--- vars.py
+++ vars.py
@@ -5,6 +5,7 @@ import os
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
 TWILIO_NUMBER = os.environ.get('TWILIO_MOBILE', None)
+TWILIO_NUMBER_US = os.environ.get('TWILIO_MOBILE_US')
 TWILIO_MSG_SVC = os.environ.get('TWILIO_MSG_SVC', None)
 SIP_NUMBER = os.environ.get('TWILIO_SIP_NUMBER', None)
 
